5446847ceda2216b1b119c507c76c472a1a7d9da
Basil Dowek <bdowek92@gmail.com>
2018-05-02 18:45:47 +0200

    AngryBirds

---
+++ AngryBirds/app/src/main/java/com/msp/AngryBirds/MainActivity.java
@@ -0,0 +1,14 @@
+package com.msp.AngryBirds;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
